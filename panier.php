6ec4cc5f6dc74290796af11ab35c692ad112a0eb
Massine <116596899+DjoudadMassine@users.noreply.github.com>
2026-03-16 09:20:28 -0400

    diddy

---
+++ panier.php
@@ -0,0 +1,19 @@
+<?php 
+session_start();
+require_once 'db.php';
+
+functi
+
+$joueur_id = $_SESSION['joueur_id'];
+$joueur_alias = $_SESSION['joueur_alias'] ;
+$joueur_or = $_SESSION['joueur_or'];
+$joueur_argent = $_SESSION['joueur_argent'];
+$joueur_bronze = $_SESSION['joueur_bronze'];
+$joueur_est_mage = $_SESSION['joueur_est_mage'];
+
+
+
+
+
+
+?>
