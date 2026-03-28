f90cb835fc6f641f302d7a35a43a78e2b8478a82
Massine <116596899+DjoudadMassine@users.noreply.github.com>
2026-03-28 18:09:00 -0400

    ????

--- paniertest.php
+++ paniertest.php
@@ -1,8 +1,7 @@
 <?php 
 session_start();
 
-
-include 'panier.php'?>
+?>
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -11,25 +10,42 @@ include 'panier.php'?>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="public/css/style.css">
         <title>paniertest.</title>
+        <style>
+            #panier-principal{
+                margin-left: 10%;
+                margin-right: 10%;
+                display:grid;
+                align-items: ;
+                border: 50px red solid;
+            }
+            #panier-items-grid{
+                display: grid;
+                width:20%;
+                height:400px;
+                border: 50px red solid;
+                flex-wrap: wrap;
+                grid-template-columns: 30% auto;
+            }
+            
+        </style>
     </head>
     <body>
-        <?php include_once 'template/header.php' ?>
-        <main>
-            <div class="cart-head">
-            <h4>Panier</h4>
-            <div id="cart-total"></div>
-      <form method="post">
-        <input type="submit" name="action" value="Acheter">
-        <input type="submit" value="Acheter">
-      </form>
-      <a class="cart-close" href="#">✕</a>
-    </div>
-    <div class="cart-items">
-      <?php include "panier.php"; ?>
-      <p>Le Panier est Vide</p>
-    </div>
-
-        </main>
+        <?php //include_once 'template/header.php' ?>
         
+        
+        <main>
+            <div class="panier-principal">
+                <h4>Panier</h4>
+                <div id="cart-total"></div>
+                <form method="post">
+                    <input type="submit" name="action" value="Acheter">
+                    <input type="submit" name="action" value="Vider">
+                </form>
+                <a class="cart-close" href="index.php">sortir</a>
+            </div>
+            <div class="panier-items">
+                <?php include "panier.php"; ?>
+            </div>
+        </main>
     </body>
 </html>
